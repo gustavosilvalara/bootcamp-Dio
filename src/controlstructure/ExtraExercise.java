5523d4b8ddf4d4cde7dc2ca129c981d2b848f440
Gustavo <gustavo.silva.lara@outlook.com>
2025-07-20 19:08:42 -0300

    Control structure exercise

---
+++ src/controlstructure/ExtraExercise.java
@@ -0,0 +1,20 @@
+package ControlStructure;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ExtraExercise {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        Random random = new Random();
+        int resRandom = random.nextInt(100);
+
+        System.out.printf("Enter the number, that divided %s the remainder is 0 \n", resRandom);
+        int attempt;
+        do {
+            attempt = s.nextInt();
+            System.out.print(resRandom % attempt == 0? "Good" : "Fail, repeat");
+        }while (!(resRandom % attempt == 0));
+    }
+}
