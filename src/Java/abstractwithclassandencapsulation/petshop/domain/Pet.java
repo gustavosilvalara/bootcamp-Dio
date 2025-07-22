aaeb62c8a014166089871c374f9167e4dcb46745
Gustavo <gustavo.silva.lara@outlook.com>
2025-07-22 02:13:47 -0300

    Created Pet Shop

---
+++ src/Java/abstractwithclassandencapsulation/petshop/domain/Pet.java
@@ -0,0 +1,8 @@
+package Java.abstractwithclassandencapsulation.petshop.domain;
+
+public record Pet(String name, String type) {
+    public Pet(String name, String type) {
+        this.name = name;
+        this.type = name;
+    }
+}
