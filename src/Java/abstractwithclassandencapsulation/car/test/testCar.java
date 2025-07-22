16a6a3a3e12ee583230b668bc4be9b2a75d5ab52
Gustavo <gustavo.silva.lara@outlook.com>
2025-07-22 00:20:41 -0300

    Created car

---
+++ src/Java/abstractwithclassandencapsulation/car/test/testCar.java
@@ -0,0 +1,43 @@
+package Java.abstractwithclassandencapsulation.car.test;
+
+import Java.abstractwithclassandencapsulation.car.domain.Car;
+
+import java.util.Scanner;
+
+public class testCar {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Scanner s = new Scanner(System.in);
+
+        int option = -1;
+
+        System.out.println("\"Enter the car\"");
+
+        do {
+            System.out.println("=======Options=======");
+            System.out.println("1 - Start the car");
+            System.out.println("2 - Turn off the car");
+            System.out.println("3 - Accelerate");
+            System.out.println("4 - Slowdown");
+            System.out.println("5 - Left");
+            System.out.println("6 - Right");
+            System.out.println("7 - Change gears");
+            System.out.println("0 - Exit");
+            System.out.println("=======Options=======");
+            System.out.println();
+
+            option = s.nextInt();
+
+            switch (option){
+                case 1 -> System.out.println(car.startTheCar());
+                case 2 -> System.out.println(car.turnOffTheCar());
+                case 3 -> System.out.println(car.accelerate());
+                case 4 -> System.out.println(car.slowDown());
+                case 5 -> System.out.println(car.left());
+                case 6 -> System.out.println(car.right());
+                case 7 -> System.out.println(car.changeGears());
+                case 0 -> System.out.println("Exiting the car");
+            }
+        }while (option != 0);
+    }
+}
