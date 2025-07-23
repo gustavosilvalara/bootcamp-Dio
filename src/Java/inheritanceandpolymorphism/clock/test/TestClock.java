edcde73824bdc04ab9ba9b4d8f263bf77751950d
Gustavo <gustavo.silva.lara@outlook.com>
2025-07-22 22:02:36 -0300

    Clock

---
+++ src/Java/inheritanceandpolymorphism/clock/test/TestClock.java
@@ -0,0 +1,8 @@
+package Java.inheritanceandpolymorphism.clock.test;
+
+import Java.inheritanceandpolymorphism.clock.domain.BRLClock;
+
+public class TestClock {
+    public static void main(String[] args) {
+    }
+}
