a9447c640102c6b91dc58d037170dbddae1bad06
Gustavo <gustavo.silva.lara@outlook.com>
2025-07-24 18:26:57 -0300

    Treinando coleções

---
+++ src/Java/collectionsandclasses/domain/Calculadora.java
@@ -0,0 +1,11 @@
+package Java.collectionsandclasses.domain;
+
+import java.util.List;
+
+public class Calculadora {
+
+    public int calcular(List<Integer> numbers){
+        return numbers.stream().reduce(0,Integer::sum);
+    }
+
+}
