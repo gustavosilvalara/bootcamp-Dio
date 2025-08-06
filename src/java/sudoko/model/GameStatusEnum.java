ab0a5d1788a2c9ac6e33546f17b9de7d1fa19d49
Gustavo <gustavo.sillva.lara@gmail.com>
2025-08-06 11:07:31 -0300

    Sudoko

---
+++ src/java/sudoko/model/GameStatusEnum.java
@@ -0,0 +1,18 @@
+package java.sudoko.model;
+
+public enum GameStatusEnum {
+
+    NON_STARTER("Não iniciado"),
+    INCOMPLETE("Incompleto"),
+    COMPLETE("Completo");
+
+    private String label;
+
+    GameStatusEnum(final String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
